0cdbc661c710e4be5e645b9d1c9fbd17e3e67d3f
MartinBock1 <martinbock@arcor.de>
2025-07-10 15:36:19 +0200

    fix: added corsheaders in settings.py

--- core/settings.py
+++ core/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -128,11 +129,15 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CSRF_TRUSTED_ORIGINS = [
     'http://127.0.0.1:5500',
     'http://localhost:5500',
+    'http://127.0.0.1:4200',
+    'http://localhost:4200',
 ]
 
 CORS_ALLOWED_ORIGINS = [
     'http://127.0.0.1:5500',
     'http://localhost:5500',
+    'http://127.0.0.1:4200',
+    'http://localhost:4200',
 ]
 
 REST_FRAMEWORK = {
